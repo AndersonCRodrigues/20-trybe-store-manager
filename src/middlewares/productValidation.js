596487336880c1709632f7d3d13638f9bb188e79
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-03-22 08:45:50 -0300

    [refactor] middleware nameValidation

--- src/middlewares/productValidation.js
+++ src/middlewares/productValidation.js
@@ -1,18 +1,21 @@
 const productSchema = require('../schemas/productSchema');
 
-const nameValidation = (req, _res, next) => {
-  const { name } = req.body;
-
-  const { error } = productSchema.validate({ name });
-  if (error.details[0].type === 'string.min') {
+const errorCheck = (error) => {
+  if (error.details[0].type === 'number.min') {
     const errType = { status: 422, message: error.message };
     throw errType;
   }
-
   if (error.details[0].type === 'any.required') {
     const errType = { status: 400, message: error.message };
     throw errType;
   }
+};
+
+const nameValidation = (req, _res, next) => {
+  const { name } = req.body;
+  const { error } = productSchema.validate({ name });
+
+  if (error) errorCheck(error);
 
   next();
 };
